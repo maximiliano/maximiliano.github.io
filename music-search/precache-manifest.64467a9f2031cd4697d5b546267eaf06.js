4eb1715c757a7a464277e0b94d84508a21ea0ae6
Maximiliano Guerra de Medeiros <maxciencomp@gmail.com>
2019-10-17 15:01:00 -0300

    Add music search project

---
+++ music-search/precache-manifest.64467a9f2031cd4697d5b546267eaf06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ae9d0bef6254bbbd88071a8ba1b96f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2edb9ffa3770d2d06305",
+    "url": "/static/css/main.300d144f.chunk.css"
+  },
+  {
+    "revision": "6d0070310b851bed984d",
+    "url": "/static/js/2.62fd152d.chunk.js"
+  },
+  {
+    "revision": "2edb9ffa3770d2d06305",
+    "url": "/static/js/main.2aeb8147.chunk.js"
+  },
+  {
+    "revision": "ea431b816b4b0231c506",
+    "url": "/static/js/runtime-main.38d9a4dc.js"
+  }
+]);
